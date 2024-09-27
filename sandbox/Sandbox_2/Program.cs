72622876511f81620ae4316e5ded623609a2b0f2
Cameron Samson <cameronwhitney150@gmail.com>
2024-09-27 11:48:29 -0600

    Changes made in class

--- sandbox/Sandbox_2/Program.cs
+++ sandbox/Sandbox_2/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+    // Computer throws 3 dice
+    Random randomGenerator = new();
+    int dice1 = randomGenerator.Nexr(1, 7);
+    int dice2 = randomGenerator.Nexr(1, 7);
+    int dice3 = randomGenerator.Nexr(1, 7);
+
     }
 }
--- sandbox/counter/Program.cs
+++ sandbox/counter/Program.cs
@@ -1,8 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Bye, World!");
+// Console.WriteLine("Bye, World!");
 
-for(int i = 0; i <= 10; i++)
-{ 
-    Console.WriteLine(i);
+// for(int i = 0; i <= 10; i++)
+// { 
+//     Console.WriteLine(i);
+// }
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numberList = new();
+
+        int input = -1;
+        while (input != 0)
+        {
+           Console.WriteLine("Enter a Number [enter a 0 to stop]:");
+           string inputString = Console.ReadLine();
+           input = int.Parse(inputString);
+           if (input != 0)
+           {
+            numberList.Add(input);
+           }
+           
+        }
+        foreach (int item in numberList)
+        {
+           Console.WriteLine(item);
+        }
+        
+    }
 }
