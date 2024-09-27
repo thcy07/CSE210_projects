440c06dcaeb6cf1b81c0e6113259e1cdcd8ba0d9
Cameron Samson <cameronwhitney150@gmail.com>
2024-09-27 11:48:00 -0600

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static double PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        string input = Console.ReadLine();
+        double number = double.Parse(input);
+        return number;
+    }
+    static double SquareNumber( double num)
+    {
+        double squareNum = num*num;
+        return squareNum;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        double userNumb = PromptUserNumber();
+        double squareNum = SquareNumber(userNumb);
+        Console.WriteLine($"{userName}, the square of you number is {squareNum}.");
+ 
+
     }
 }
