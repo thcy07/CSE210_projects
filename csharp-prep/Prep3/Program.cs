377e830dc8e51b96f2f6569221eb9d090eae64be
Cameron Samson <cameronwhitney150@gmail.com>
2024-09-26 22:58:05 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+       
+       string guess, response = "";
+       int guessNumber = 0, number;
+       int attempts;
+       do
+       {        
+        Random randomGenerator = new Random(); 
+        number = randomGenerator.Next(1,101);
+        attempts = 0;
+            while (guessNumber != number)
+            {   
+                attempts++;
+                Console.WriteLine(number);
+                Console.Write("What is the magic number:");
+                guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                if (guessNumber > number)
+                { Console.WriteLine("Lower");}
+                else if (guessNumber < number)
+                { Console.WriteLine("Higher"); }
+                else
+                { Console.WriteLine($"You Guessed it! In {attempts} attempts!"); 
+                Console.Write("Do you want to continue? ");
+                response = Console.ReadLine();
+                }
+            }   
+       
+       } while (response == "Yes" || response == "yes");
     }
 }
