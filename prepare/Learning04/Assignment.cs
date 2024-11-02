0d157f5e62cc1351a2b3b7ce075f50332a775912
Cameron Samson <cameronwhitney150@gmail.com>
2024-11-01 21:04:14 -0600

    Learning04

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,31 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    // We will provide Getters for our private member variables so they can be accessed
+    // later both outside the class as well is in derived classes.
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
