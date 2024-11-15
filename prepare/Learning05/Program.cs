4f5ec4884a0e80ce0d989bf8acaab9746384e0f3
Cameron Samson <cameronwhitney150@gmail.com>
2024-11-15 11:13:37 -0700

    Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Green", 3);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("Red", 3, 4);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("Blue", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
